9db44165b790835b576d36c9cbcefdafcfe12b99
GoulartL <alissongluck@gmail.com>
2021-12-07 13:35:30 -0300

    Uptade utils.js

--- api/util.js
+++ api/util.js
@@ -5,5 +5,9 @@ const { TOKEN_SECRET } = require("./env");
 exports.authMiddleware = expressJwt({ secret: TOKEN_SECRET, algorithms: ["HS256"] });
 
 exports.encryptSHA256 = function(input) {
-    return crypto.createHash("sha256").update(input).digest("hex");
+    if (typeof input != 'undefined') {
+        return crypto.createHash("sha256").update(input).digest("hex");
+    } else {
+        return false;
+    }
 };
